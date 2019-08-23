e75ec65335995cdb194d14ad13f9bf4782ed6b83
Luis E. Lopez <luis02lopez@hotmail.com>
2019-08-22 22:28:23 -0500

    Adding GUI example

---
+++ activity-1/GuideAcceptParamsGenPlotsExample.m
@@ -0,0 +1,15 @@
+%% Open and Run the Example
+% Open and run the app. Change the default values in the *f1* and *f2* 
+% fields to change the frequency for each component sine wave. You can also change 
+% the three numbers (separated by colons) in the *t* field. The first and 
+% last numbers specify the window of time to sample the function. The middle 
+% number specifies the sampling rate.
+%
+% Press the *Plot* button to see the graph of the function in the frequency
+% and time domains.  
+%
+% <<../guidetimefreqplot.png>>
+%
+
+%% 
+% Copyright 2012 The MathWorks, Inc.
